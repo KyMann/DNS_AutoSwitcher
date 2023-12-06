83d47f3d9d615075d568ebedd512985279b4680b
Kymann <akavir7@gmail.com>
2023-12-05 22:24:43 -0600

    added fetch-retry and a function/route pair to handle duckdns updates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import express from 'express';
 import path from 'path';
 
+const fetch = require('fetch-retry');
+
 import logger from './logger';
 
 const PORT = 7878;
+const DNSHOST = "https://www.duckdns.org/update?domains=piecraft";
+const DUCKTOKEN = "";
+
+const DNSUpdate = async (newIP="") => {
+    const url = `${DNSHOST}&token=${DUCKTOKEN}&ip=${newIP}`; 
+    //DUCK DNS will automatically detect your remote ip if you leave the field blank
+    const DNSUpdateResponse = await fetch(
+        url, {
+            method: 'GET',
+            retryOn: [502]
+        }
+    );
+    if (DNSUpdateResponse.status === 200) {
+        logger.debug('updated ip');
+    } else { 
+        throw new Error(`Recieved response status ${DNSUpdateResponse.status} trying to resolve ${url}`);
+    }
+};
+
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,10 +35,10 @@ app.get('/', (request, response) => {
     logger.debug('home page was visited');
 });
 
-app.get('/status', async (request, response) => {
-    const result = 'Server Is Up';
+app.get('/update', async (request, response) => {
+    await DNSUpdate();
     //TODO: add dynamic server status
-    logger.debug('updated status');
+    logger.debug('update successful');
 })
 
 app.listen(PORT, (err) => {
